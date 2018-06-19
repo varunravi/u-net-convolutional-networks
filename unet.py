d30b7b2e710a458fa7e7823db9a01ce13b4dd7b0
System Administrator <root@Varuns-MacBook-Pro.local>
2018-06-19 09:22:57 -0700

    not done

--- unet.py
+++ unet.py
@@ -27,46 +27,46 @@ def unet(input_shape):
 	
 	inputs = Input(shape=input_shape)
 
-	down_conv1 = Conv2D(64, (3,3), padding = 'same', input_shape=input_shape, activation='relu')(inputs)
-	down_conv1 = Conv2D(64, (3,3), padding = 'same', activation='relu')(down_conv1)
-	
+	down_conv1 = Conv2D(64, (3,3), padding = 'valid', input_shape=input_shape, activation='relu')(inputs)
+	down_conv1 = Conv2D(64, (3,3), padding = 'valid', activation='relu')(down_conv1)
+
 	down_conv2 = MaxPooling2D(pool_size=(2,2), strides=2)(down_conv1)
-	down_conv2 = Conv2D(128, (3,3), padding = 'same', activation='relu')(down_conv2)
-	down_conv2 = Conv2D(128, (3,3), padding = 'same', activation='relu')(down_conv2)
+	down_conv2 = Conv2D(128, (3,3), padding = 'valid', activation='relu')(down_conv2)
+	down_conv2 = Conv2D(128, (3,3), padding = 'valid', activation='relu')(down_conv2)
 	
 	down_conv3 = MaxPooling2D(pool_size=(2,2), strides=2)(down_conv2)
-	down_conv3 = Conv2D(256, (3,3), padding = 'same', activation='relu')(down_conv3)
-	down_conv3 = Conv2D(256, (3,3), padding = 'same', activation='relu')(down_conv3)
+	down_conv3 = Conv2D(256, (3,3), padding = 'valid', activation='relu')(down_conv3)
+	down_conv3 = Conv2D(256, (3,3), padding = 'valid', activation='relu')(down_conv3)
 	
 	down_conv4 = MaxPooling2D(pool_size=(2,2), strides=2)(down_conv3)
-	down_conv4 = Conv2D(512, (3,3), padding = 'same', activation='relu')(down_conv4)
-	down_conv4 = Conv2D(512, (3,3), padding = 'same', activation='relu')(down_conv4)
+	down_conv4 = Conv2D(512, (3,3), padding = 'valid', activation='relu')(down_conv4)
+	down_conv4 = Conv2D(512, (3,3), padding = 'valid', activation='relu')(down_conv4)
 	
 	down_conv5 = MaxPooling2D(pool_size=(2,2), strides=2)(down_conv4)
-	down_conv5 = Conv2D(1024, (3,3), padding = 'same', activation='relu')(down_conv5)
-	down_conv5 = Conv2D(1024, (3,3), padding = 'same', activation='relu')(down_conv5)
-	
+	down_conv5 = Conv2D(1024, (3,3), padding = 'valid', activation='relu')(down_conv5)
+	down_conv5 = Conv2D(1024, (3,3), padding = 'valid', activation='relu')(down_conv5)
+
 	up_conv1 = UpSampling2D(size=(2,2))(down_conv5)
 	up_conv1 = concatenate([down_conv4, up_conv1], axis=-1)
-	up_conv1 = Conv2D(512, (3,3), padding = 'same', activation='relu')(up_conv1)
-	up_conv1 = Conv2D(512, (3,3), padding = 'same', activation='relu')(up_conv1)
+	up_conv1 = Conv2D(512, (3,3), padding = 'valid', activation='relu')(up_conv1)
+	up_conv1 = Conv2D(512, (3,3), padding = 'valid', activation='relu')(up_conv1)
 
 	up_conv2 = UpSampling2D(size=(2,2))(up_conv1)
 	up_conv2 = concatenate([down_conv3, up_conv2], axis=-1)
-	up_conv2 = Conv2D(256, (3,3), padding = 'same', activation='relu')(up_conv2)
-	up_conv2 = Conv2D(256, (3,3), padding = 'same', activation='relu')(up_conv2)
+	up_conv2 = Conv2D(256, (3,3), padding = 'valid', activation='relu')(up_conv2)
+	up_conv2 = Conv2D(256, (3,3), padding = 'valid', activation='relu')(up_conv2)
 
 	up_conv3 = UpSampling2D(size=(2,2))(up_conv2)
 	up_conv3 = concatenate([down_conv2, up_conv3], axis=-1)
-	up_conv3 = Conv2D(128, (3,3), padding = 'same', activation='relu')(up_conv3)
-	up_conv3 = Conv2D(128, (3,3), padding = 'same', activation='relu')(up_conv3)	
+	up_conv3 = Conv2D(128, (3,3), padding = 'valid', activation='relu')(up_conv3)
+	up_conv3 = Conv2D(128, (3,3), padding = 'valid', activation='relu')(up_conv3)	
 
 	up_conv4 = UpSampling2D(size=(2,2))(up_conv3)
 	up_conv4 = concatenate([down_conv1, up_conv4], axis=-1)
-	up_conv4 = Conv2D(64, (3,3), padding = 'same', activation='relu')(up_conv4)
-	up_conv4 = Conv2D(64, (3,3), padding = 'same', activation='relu')(up_conv4)
+	up_conv4 = Conv2D(64, (3,3), padding = 'valid', activation='relu')(up_conv4)
+	up_conv4 = Conv2D(64, (3,3), padding = 'valid', activation='relu')(up_conv4)
 
-	final = Conv2D(1, 1, padding = 'same', activation='softmax')(up_conv4)
+	final = Conv2D(1, 1, padding = 'valid', activation='softmax')(up_conv4)
 
 	model = Model(inputs=inputs, outputs=final)
 	model.compile(optimizer=SGD(lr = 1e-4), loss='binary_crossentropy', metrics=['accuracy'])
@@ -80,17 +80,15 @@ if __name__ == '__main__':
 	Y_train = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 	list_img = pywalker('./letters')
 
-	model = unet((64, 64, 1))
+	model = unet((572, 572, 1))
 	model.summary()
 	
 	for i in list_img:
 		X_train.append(mpimg.imread(i))
-	ipdb.set_trace()
-
+	
 	X_train = np.array(X_train)
 
-	f = 4
-	
+	ipdb.set_trace()
 
 	
 	
