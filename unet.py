81bca7435bcdcb4596d5ce3456c9d5fdbeb137ba
Varun Ravi <rvarun144@gmail.com>
2018-07-25 00:28:04 -0700

    unet done

--- unet.py
+++ unet.py
@@ -1,209 +1,154 @@
-# Varun Ravi
-# unet.py
-
-import numpy as np
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-import ipdb
-import os
 import tensorflow as tf
+from keras.datasets import cifar10
+import numpy as np
 
-X_SHAPE = [None,32,32,3]
-y_SHAPE = [None,100]
-EPOCHS = 1
-DATA = 'cifar10'
-BATCH_SIZE = 8
-DATA = 'cifar100'
+EPOCH = 5
+BATCH_SIZE = 64
 
-def get_data(data):
+def conv2d(layer, w_name, w_shape, b_name, b_shape):
+ 
+  # with tf.Graph().as_default() as graph:
+  w = tf.get_variable(name=w_name, shape=w_shape, dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+  b = tf.get_variable(name=b_name, shape=b_shape, dtype=tf.float32, initializer=tf.zeros_initializer())
+  layer = tf.nn.conv2d(layer, w, [1, 1, 1, 1], padding='SAME')
+  layer = tf.add(b, layer)
 
-	if data == 'cifar10':
-		(train_data, train_labels), (eval_data, eval_labels) = tf.keras.datasets.cifar10.load_data()
-		train_labels = one_hot(train_labels, (50000,10))
-		eval_labels = one_hot(eval_labels, (10000,10))
-
-	if data == 'cifar100':
-		(train_data, train_labels), (eval_data, eval_labels) = tf.keras.datasets.cifar10.load_data()
-		train_labels = one_hot(train_labels, (50000,100))
-		eval_labels = one_hot(eval_labels, (50000,100))
+  return layer
 
 
-	return train_data, train_labels, eval_data, eval_labels
+def up_conv2d(layer, w_name, w_shape, b_name, b_shape):
+ 
+  w = tf.get_variable(name=w_name, shape=w_shape, dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+  b = tf.get_variable(name=b_name, shape=b_shape, dtype=tf.float32, initializer=tf.zeros_initializer())
 
-def one_hot(labels, shape):
-	one_hot= np.zeros(shape)
-	pos=0
+  current_shape_size = layer.shape
+  output_shape=[BATCH_SIZE] + [int(current_shape_size[1].value*2),int(current_shape_size[2].value*2),int(current_shape_size[3].value/2)]
+  layer = tf.nn.conv2d_transpose(layer, w, output_shape, [1, 2, 2, 1], padding='SAME')
 
-	for i in labels:
-		one_hot[pos][i] = 1
-		pos+=1
+  layer = tf.add(b, layer)
 
-	return one_hot
-
-def pywalker(path):
-	
-	list_files = []
-
-	for root, dirs, files in os.walk(path):
- 		for file_ in files:
- 			if file_ != '.DS_Store':
- 				list_files.append(os.path.join(root, file_))
-
-	return list_files
-
-def conv2d_transpose(x):
-	x_shape = tf.shape(x)
-	output_shape = tf.stack([x_shape[0], x_shape[1]*2, x_shape[2]*2, x_shape[3]//2])
-	
-	return tf.nn.conv2d_transpose(x, (3,3, 1, 1), output_shape, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def down_block(inputs, filters, kernel_size=(3,3), padding='same', strides=(2,2), activation=tf.nn.relu, max_pool_size=(2,2), max_strides=2):
-
-	inputs = tf.layers.max_pooling2d(inputs, max_pool_size, strides, padding)
-	inputs = tf.layers.conv2d(inputs, filters, kernel_size, padding=padding, activation=activation)
-	inputs = tf.layers.conv2d(inputs, filters, kernel_size, padding=padding, activation=activation)
-
-	return inputs
-
-def up_block(inputs, parallel_down_conv, filters, kernel_size=(3,3), padding='same', activation=tf.nn.relu):
-
-	inputs = tf.layers.conv2d_transpose(inputs, filters, kernel_size, padding=padding)
-	inputs = tf.image.resize_nearest_neighbor(inputs, (inputs.get_shape().as_list()[1]*2,inputs.get_shape().as_list()[2]*2))
-
-	inputs = tf.concat([parallel_down_conv[:,:inputs.get_shape().as_list()[1],:inputs.get_shape().as_list()[2],:], inputs], axis=-1)
-	
-	inputs = tf.layers.conv2d(inputs, 512, (3,3), activation=activation,  padding='same')
-	inputs = tf.layers.conv2d(inputs, 512, (3,3), activation=activation,  padding='same')
-
-	return inputs
-
-def model(x,y):
-
-	down_conv1 = tf.layers.conv2d(x, 64, (3,3), activation=tf.nn.relu, padding='same')
-	down_conv1 = tf.layers.conv2d(down_conv1, 64, (3,3), activation=tf.nn.relu, padding='same')
-	
-	down_conv2 = down_block(down_conv1, 128)
-	down_conv3 = down_block(down_conv2, 256)
-	down_conv4 = down_block(down_conv3, 512)
-	down_conv5 = down_block(down_conv4, 1024)
-
-	up_conv1 = up_block(down_conv5, down_conv4, 512)
-	up_conv2 = up_block(up_conv1, down_conv3, 256)
-	up_conv3 = up_block(up_conv2, down_conv2, 128)
-	up_conv4 = up_block(up_conv3, down_conv1, 64)
-	#ipdb.set_trace()
-
-	flatten_layer = tf.contrib.layers.flatten(up_conv4)
-	final_layer = tf.layers.dense(inputs=flatten_layer, units=100)
-	
-	return final_layer
+  return layer
 
 
 if __name__ == '__main__':
 
-	# dimensions
-	x = tf.placeholder(tf.float32, shape=X_SHAPE, name='x')
-	y = tf.placeholder(tf.float32, shape=y_SHAPE, name='y')
+	(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+	
+	tf.reset_default_graph()
 
-	train_data, train_labels, eval_data, eval_labels = get_data(DATA)
+	x = tf.placeholder(dtype=tf.float32, shape=[None, 32, 32, 3], name='input')
+	y = tf.placeholder(dtype=tf.int32, shape=[None, 1], name = 'y')
 
-	final = model(x, y)
-	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=final, labels=y))
-	optimize = tf.train.GradientDescentOptimizer(learning_rate=1e-4).minimize(loss)
+	layer = conv2d(x, 'w0', [3, 3, 3, 64], 'b0', [64])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w1', [3, 3, 64, 64], 'b1', [64])
+	layer_1 = tf.nn.relu(layer, name='conv_1')
 
-	# training
-	init = tf.global_variables_initializer()	
+	layer = tf.nn.max_pool(value=layer_1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='maxpool_1')
+
+	layer = conv2d(layer, 'w2', [3, 3, 64, 128], 'b2', [128])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w3', [3, 3, 128, 128], 'b3', [128])
+	layer_2 = tf.nn.relu(layer, name='conv_2')
+
+	layer = tf.nn.max_pool(value=layer_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='maxpool_2')
+
+	layer = conv2d(layer, 'w4', [3, 3, 128, 256], 'b4', [256])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w5', [3, 3, 256, 256], 'b5', [256])
+	layer_3 = tf.nn.relu(layer, name='conv_3')
+
+	layer = tf.nn.max_pool(value=layer_3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='maxpool_2')
+
+	layer = conv2d(layer, 'w6', [3, 3, 256, 512], 'b6', [512])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w7', [3, 3, 512, 512], 'b7', [512])
+	layer_4 = tf.nn.relu(layer, name='conv_4')
+
+	layer = tf.nn.max_pool(value=layer_4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='maxpool_3')
+
+	layer = conv2d(layer, 'w8', [3, 3, 512, 1024], 'b8', [1024])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w9', [3, 3, 1024, 1024], 'b9', [1024])
+	layer = tf.nn.relu(layer)
+
+	layer = up_conv2d(layer, 'w10', [2, 2, 512, 1024], 'b10', [512])
+	layer = tf.concat([layer_4, layer], axis=3)
+
+	layer = conv2d(layer, 'w11', [3, 3, 1024, 512], 'b11', [512])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w12', [3, 3, 512, 512], 'b12', [512])
+	layer = tf.nn.relu(layer)
+
+	layer = up_conv2d(layer, 'w13', [2, 2, 256, 512], 'b13', [256])
+	layer = tf.concat([layer_3, layer], axis=3)
+
+	layer = conv2d(layer, 'w14', [3, 3, 512, 256], 'b14', [256])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w15', [3, 3, 256, 256], 'b15', [256])
+	layer = tf.nn.relu(layer)
+
+	layer = up_conv2d(layer, 'w16', [2, 2, 128, 256], 'b16', [128])
+	layer = tf.concat([layer_2, layer], axis=3)
+
+	layer = conv2d(layer, 'w17', [3, 3, 256, 128], 'b17', [128])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w18', [3, 3, 128, 128], 'b18', [128])
+	layer = tf.nn.relu(layer)
+
+	layer = up_conv2d(layer, 'w19', [2, 2, 64, 128], 'b19', [64])
+	layer = tf.concat([layer_1, layer], axis=3)
+
+	layer = conv2d(layer, 'w20', [3, 3, 128, 64], 'b20', [64])
+	layer = tf.nn.relu(layer)
+	layer = conv2d(layer, 'w21', [3, 3, 64, 64], 'b21', [64])
+	layer = tf.nn.relu(layer)
+
+	layer = conv2d(layer, 'w22', [1, 1, 64, 1], 'b22', [1])
+
+	layer = tf.contrib.layers.flatten(inputs=layer)
+	w_flat = tf.get_variable(name='w_flat', shape=[layer.shape[1], 10], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+	b_flat = tf.get_variable(name='b_flat', shape=[10], dtype=tf.float32, initializer=tf.zeros_initializer())
+	layer = tf.matmul(layer, w_flat)
+	final = tf.add( name='cifar10_output', x=b_flat, y=layer)
+
+	# loss
+	global_step = tf.train.get_or_create_global_step()
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.one_hot(tf.cast(y, dtype=tf.int32), 10), logits=final))
+	optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, global_step=global_step)
+
+	# prediction
+	softmax = tf.argmax(tf.nn.softmax(logits=final), axis=1, output_type=tf.int32)
+	re = tf.reshape(softmax, [BATCH_SIZE, 1])
+	prediction = tf.reduce_mean(tf.cast(tf.equal(re, y), dtype=tf.float32))
+
+
 	saver = tf.train.Saver()
-	config = tf.ConfigProto()
-	#config.gpu_options.allow_growth = True
-	config.log_device_placement=True
-	#config.gpu_options.per_process_gpu_memory_fraction = 0.7
 
-	with tf.Session(config=config) as session:
-		session.run(init)
-		for epoch in range(0, EPOCHS):
-			total_steps = int(len(train_data)/BATCH_SIZE)
+	with tf.Session() as sess:
 
-			total_loss, _ = session.run([loss, optimize], feed_dict={x:train_data[:500],y:train_labels[:500]})
-			print("epochs: %d, loss: %f" % (epoch, np.mean(total_loss)))
+		sess.run(tf.global_variables_initializer())
+		#saver.restore(sess, tf.train.latest_checkpoint('./cifar-10'))
 
-			# for step in range(200):
-			# 	start = step*BATCH_SIZE
-			# 	end = start+BATCH_SIZE
-			# 	X_batch = train_data[start:end]
-			# 	y_batch = train_labels[start:end]
-			# 	_, training_loss = session.run([optimize, loss], feed_dict={x: X_batch, y: y_batch})
-				
-			# 	print ("Steps %d/%d, Training Loss=%f" % (step+1, total_steps, training_loss))
+		for epoch in range(EPOCH):
+			total_steps = int(x_train.shape[0]/BATCH_SIZE)
+			for step in range(total_steps):
+				x_batch = x_train[step*BATCH_SIZE:step*BATCH_SIZE + BATCH_SIZE].reshape(BATCH_SIZE, 32, 32, 3)
+				y_batch = y_train[step*BATCH_SIZE:step*BATCH_SIZE + BATCH_SIZE].reshape(BATCH_SIZE, 1)
 
-			total_steps = int(len(eval_data)/BATCH_SIZE)
-			total_loss = 0
-			for step in range(200):
-				start = step*BATCH_SIZE
-				end = start + BATCH_SIZE
-				X_batch = eval_data[start:end]
-				yers.conv2d(inputs, 512, (3,3), activation=activation,  padding='same')
-	inputs = tf.layers.conv2d(inputs, 512, (3,3), activation=activation,  padding='same')
+				current_global_step, _, current_loss= sess.run([global_step, optimizer, loss], feed_dict={x:x_batch, y:y_batch})
 
-	return inputs
+				print("epoch: %d global_step: %d, step: %d loss: %f" % (epoch+1, current_global_step, step, current_loss))
 
-def model(x,y):
+				if step % 10 == 0:
+					xtest_batch = x_test[:BATCH_SIZE].reshape(BATCH_SIZE, 32, 32, 3)
+					ytest_batch = y_test[:BATCH_SIZE].reshape(BATCH_SIZE, 1)
+					softmax_p, accuracy = sess.run([softmax, prediction], feed_dict={x: xtest_batch, y: ytest_batch})
 
-	down_conv1 = tf.layers.conv2d(x, 64, (3,3), activation=tf.nn.relu, padding='same')
-	down_conv1 = tf.layers.conv2d(down_conv1, 64, (3,3), activation=tf.nn.relu, padding='same')
-	
-	down_conv2 = down_block(down_conv1, 128)
-	down_conv3 = down_block(down_conv2, 256)
-	down_conv4 = down_block(down_conv3, 512)
-	down_conv5 = down_block(down_conv4, 1024)
-
-	up_conv1 = up_block(down_conv5, down_conv4, 512)
-	up_conv2 = up_block(up_conv1, down_conv3, 256)
-	up_conv3 = up_block(up_conv2, down_conv2, 128)
-	up_conv4 = up_block(up_conv3, down_conv1, 64)
-	#ipdb.set_trace()
-
-	flatten_layer = tf.contrib.layers.flatten(up_conv4)
-	final_layer = tf.layers.dense(inputs=flatten_layer, units=100)
-	
-	return final_layer
+					print("accuracy: %f%%" % (accuracy*100))
+	    
+			saver.save(sess, save_path='./cifar-10/model.chkpt', global_step=current_global_step)
+		tf.train.write_graph(sess.graph, './exported_graph/cifar-10', 'final.pb', as_text=False)
+		tf.train.export_meta_graph('./exported_graph/cifar-10/final.meta', graph=graph, clear_devices=True)
 
 
-if __name__ == '__main__':
-
-	# dimensions
-	x = tf.placeholder(tf.float32, shape=X_SHAPE, name='x')
-	y = tf.placeholder(tf.float32, shape=y_SHAPE, name='y')
-
-	train_data, train_labels, eval_data, eval_labels = get_data(DATA)
-
-	final = model(x, y)
-	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=final, labels=y))
-	optimize = tf.train.GradientDescentOptimizer(learning_rate=1e-4).minimize(loss)
-
-	# training
-	init = tf.global_variables_initializer()	
-	saver = tf.train.Saver()
-	config = tf.ConfigProto()
-	#config.gpu_options.allow_growth = True
-	config.log_device_placement=True
-	#config.gpu_options.per_process_gpu_memory_fraction = 0.7
-
-	with tf.Session(config=config) as session:
-		session.run(init)
-		for epoch in range(0, EPOCHS):y_batch = eval_labels[start:end]
-				valid_loss = session.run(loss, feed_dict={x: X_batch, y: y_batch})
-				total_loss += valid_loss
-				print ("Steps %d/%d, Validation Loss=%f" % (step+1, total_steps, valid_loss))
-			valid_loss = total_loss / total_steps
-			ipdb.set_trace()
-			print ("Epoch %d, Training Loss = %d, Validation Loss = %d" % (epoch, training_loss, valid_loss))
-
-		saver.save(session, './dpn_results-tf')	
-
-
-
-	
-
-	
